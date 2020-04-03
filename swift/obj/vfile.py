65e7eb2cdbbddda56b95945882cc06695ffc6001
Alexandre Lécuyer <alexandre.lecuyer@corp.ovh.com>
2020-04-03 10:50:01 +0200

    Use hashes.pkl files for LOSF and remove list_partition_recursive()
    This removes LOSF specific code to speed up __get_hashes() and
    yield_hashes(). We now inherit diskfile code, and where not possible,
    copy it with minimal changes to make it work: change os.* calls to the
    LOSF equivalent.
    
    The performance impact is mitigated by the use of hashes.pkl files which
    were previously disabled for LOSF. Further patches to the index-server
    key-value layout will also make listing suffix content much cheaper.
    
    Change-Id: I3cb1fcffc8a09c356ed0e9f5e10f78ddc15cda9f

--- swift/obj/vfile.py
+++ swift/obj/vfile.py
@@ -833,29 +833,6 @@ def list_quarantined_ohash(quarantined_ohash_path):
     return rpc.list_quarantined_ohash(si.socket_path, si.ohash)
 
 
-def build_partition_tree(path):
-    """
-    :param path: full path to partition
-    :return: a dict of dict of the partition directory tree:
-    suffix -> object hash -> list of filenames
-    """
-    path = os.path.normpath(path)
-    si = SwiftPathInfo.from_path(path)
-
-    if not POLICIES[si.policy_idx].object_ring:
-        POLICIES[si.policy_idx].load_ring('/etc/swift')
-    part_power = 32 - POLICIES[si.policy_idx].object_ring._part_shift
-
-    partition_tree = defaultdict(lambda: defaultdict(list))
-    full_path_entries = rpc.list_partition_recursive(si.socket_path,
-                                                     si.partition, part_power)
-    for entry in full_path_entries:
-        partition_tree[str(entry.suffix)][str(entry.ohash)].append(
-            str(entry.filename))
-
-    return partition_tree
-
-
 def _list_ohash(si, part_power):
     """
     :param si: SwiftPathInfo object
